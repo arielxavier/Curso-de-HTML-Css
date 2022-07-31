f6660cc4afd24b6854ec9e4b593323676037b17e
euarielxavier <arieli.tdb@gmail.com>
2022-07-31 20:58:39 -0300

    primeiro modulo html guanabara

---
+++ modulo 01/exercicios/ex002-paragrafos,quebras/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+     public static void main(String[] args) {
+    System.out.println("Hello World");
+
+    /*Para criar uma variável, você deve especificar o tipo e atribuir um valor a ela:*/
+    String name = "John";
+    System.out.println(name);
+
+    int myNumb = 15;
+    System.out.println(myNumb);
+
+  }
+  }
